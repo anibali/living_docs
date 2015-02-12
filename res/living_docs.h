5bd9cb2f5ef9ad88f77278970da15c0ffe1a8a74
Aiden Nibali <dismaldenizen@gmail.com>
2015-02-12 22:01:17 +1100

    List failed example checks

--- res/living_docs.h
+++ res/living_docs.h
@@ -2,9 +2,17 @@
 #define __LIVING_DOCS_H__
 
 #include "stdio.h"
+#include "stdlib.h"
 
-#define __do_assert(e, file, line) \
-    ((void)printf("%s:%u: failed assertion `%s'\n", file, line, e))
+char** __failure_messages;
+int __n_failure_messages;
+
+#define __do_assert(e, file, line) {                                          \
+  char* __failure_message = malloc(sizeof(char) * 256);                       \
+  sprintf(__failure_message, "%s:%u: failed assertion `%s'", file, line, e);  \
+  __failure_messages[__n_failure_messages++] = __failure_message;             \
+}
+
 #define do_assert(EX) {if(!(EX)) { (__do_assert(#EX, __FILE__, __LINE__)); return 1; }}
 
 #define will_be_true(expr) do_assert(expr)
