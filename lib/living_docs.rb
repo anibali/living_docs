869bb5d70d9b5faae35ae4885726f6c659ffaf38
Aiden Nibali <dismaldenizen@gmail.com>
2015-02-22 14:40:18 +1100

    Rearranged options

--- lib/living_docs.rb
+++ lib/living_docs.rb
@@ -12,13 +12,13 @@ module LivingDocs
         cflags: ""
       }
 
-      input_dir = "."
+      output_dir = "doc/"
 
       opts = OptionParser.new do |opts|
         opts.banner = <<-BANNER.gsub(/^ {10}/, "")
           Generate documentation and executable examples for a C project
 
-          livdoc [options] <output_dir>
+          livdoc [options] [input_dir=.]
 
           Examples:
             Generate docs from code in ./include and ./src, putting the results
@@ -31,19 +31,19 @@ module LivingDocs
         opts.on("-h", "--help", "Show this help message") {puts opts; exit}
         opts.on("-v", "--version", "Show version") {puts VERSION; exit}
         opts.on("-n", "--no-examples", "Do not compile examples") {|c| options[:compile_examples] = false}
-        opts.on("-i", "--input INPUT_DIR", "Input directory") {|c| input_dir = c}
+        opts.on("-o", "--output OUTPUT_DIR", "Output directory") {|c| output_dir = c}
         opts.on("--compiler COMPILER", "Compiler (default=clang)") {|c| options[:compiler] = c}
         opts.on("--cflags CFLAGS", "Compiler flags") {|c| options[:cflags] = c}
       end
 
       opts.parse!(argv)
 
-      unless argv.size == 1
+      unless argv.size <= 1
         puts opts
         exit(1)
       end
 
-      output_dir = argv.last
+      input_dir = argv.empty? ? "." : argv[0]
 
       [input_dir, output_dir, options]
     end
